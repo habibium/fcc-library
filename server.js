5375095fbab13dac359e6624d9cbac9d37979e7e
habibium <habib.prodev@gmail.com>
2024-10-20 20:42:10 +0600

    refactor: Remove commented out code in server.js

--- server.js
+++ server.js
@@ -65,19 +65,5 @@ listen(+process.env.PORT || 3000)
     }
   })
   .catch(console.error);
-// const listener = app.listen(process.env.PORT || 3000, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-//   if (process.env.NODE_ENV === "test") {
-//     console.log("Running Tests...");
-//     setTimeout(function () {
-//       try {
-//         runner.run();
-//       } catch (e) {
-//         console.log("Tests are not valid:");
-//         console.error(e);
-//       }
-//     }, 1500);
-//   }
-// });
 
 module.exports = app; //for unit/functional testing
